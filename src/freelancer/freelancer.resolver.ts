a614fe3e0ccb8b4e3c47d27b34d5a4226a16f031
mansour123456789 <bouchniba1997.mansour@gmail.com>
2025-06-07 18:54:32 +0200

    les service de freelancer

--- src/freelancer/freelancer.resolver.ts
+++ src/freelancer/freelancer.resolver.ts
@@ -1,35 +1,39 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { FreelancerService } from './freelancer.service';
 import { Freelancer } from './entities/freelancer.entity';
 import { CreateFreelancerInput } from './dto/create-freelancer.input';
 import { UpdateFreelancerInput } from './dto/update-freelancer.input';
+import { FreelancerService } from './freelancer.service';
 
 @Resolver(() => Freelancer)
 export class FreelancerResolver {
   constructor(private readonly freelancerService: FreelancerService) {}
 
-  @Mutation(() => Freelancer)
-  createFreelancer(@Args('createFreelancerInput') createFreelancerInput: CreateFreelancerInput) {
-    return this.freelancerService.create(createFreelancerInput);
-  }
-
-  @Query(() => [Freelancer], { name: 'freelancer' })
-  findAll() {
+  @Query(() => [Freelancer])
+  async freelancers(): Promise<Freelancer[]> {
     return this.freelancerService.findAll();
   }
 
-  @Query(() => Freelancer, { name: 'freelancer' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  @Query(() => Freelancer, { nullable: true })
+  async freelancer(@Args('id', { type: () => Int }) id: number): Promise<Freelancer> {
     return this.freelancerService.findOne(id);
   }
 
   @Mutation(() => Freelancer)
-  updateFreelancer(@Args('updateFreelancerInput') updateFreelancerInput: UpdateFreelancerInput) {
+  async createFreelancer(
+    @Args('createFreelancerInput') createFreelancerInput: CreateFreelancerInput,
+  ): Promise<Freelancer> {
+    return this.freelancerService.create(createFreelancerInput);
+  }
+
+  @Mutation(() => Freelancer)
+  async updateFreelancer(
+    @Args('updateFreelancerInput') updateFreelancerInput: UpdateFreelancerInput,
+  ): Promise<Freelancer> {
     return this.freelancerService.update(updateFreelancerInput.id, updateFreelancerInput);
   }
 
   @Mutation(() => Freelancer)
-  removeFreelancer(@Args('id', { type: () => Int }) id: number) {
+  async removeFreelancer(@Args('id', { type: () => Int }) id: number): Promise<Freelancer> {
     return this.freelancerService.remove(id);
   }
 }
--- src/freelancer/freelancer.service.ts
+++ src/freelancer/freelancer.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Freelancer } from './entities/freelancer.entity';
 import { CreateFreelancerInput } from './dto/create-freelancer.input';
 import { UpdateFreelancerInput } from './dto/update-freelancer.input';
 
 @Injectable()
 export class FreelancerService {
-  create(createFreelancerInput: CreateFreelancerInput) {
-    return 'This action adds a new freelancer';
+  constructor(
+    @InjectRepository(Freelancer)
+    private freelancerRepository: Repository<Freelancer>,
+  ) {}
+
+  async create(createFreelancerInput: CreateFreelancerInput): Promise<Freelancer> {
+    const freelancer = this.freelancerRepository.create(createFreelancerInput);
+    return this.freelancerRepository.save(freelancer);
   }
 
-  findAll() {
-    return `This action returns all freelancer`;
+  async findAll(): Promise<Freelancer[]> {
+    return this.freelancerRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} freelancer`;
+  async findOne(id: number): Promise<Freelancer> {
+    const freelancer = await this.freelancerRepository.findOne({ where: { id } });
+    if (!freelancer) {
+      throw new NotFoundException(`Freelancer #${id} not found`);
+    }
+    return freelancer;
   }
 
-  update(id: number, updateFreelancerInput: UpdateFreelancerInput) {
-    return `This action updates a #${id} freelancer`;
+  async update(id: number, updateFreelancerInput: UpdateFreelancerInput): Promise<Freelancer> {
+    const freelancer = await this.findOne(id);
+    Object.assign(freelancer, updateFreelancerInput);
+    return this.freelancerRepository.save(freelancer);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} freelancer`;
+  async remove(id: number): Promise<Freelancer> {
+    const freelancer = await this.findOne(id);
+    return this.freelancerRepository.remove(freelancer);
   }
 }
