669319ef1c91c3c4eb48ebc52a8eb308915c8a4b
mansour123456789 <bouchniba1997.mansour@gmail.com>
2025-06-07 18:50:37 +0200

    update les entity rt ajouter les relation et le add le resolver

--- src/freelancer/entities/freelancer.entity.ts
+++ src/freelancer/entities/freelancer.entity.ts
@@ -1,6 +1,8 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToMany, JoinTable } from 'typeorm';
-
+import { Competence } from '../competence/competence.entity';
+import { LienProfessionnel } from '../link/link.entity';
+import { Mission } from '../mission/mission.entity';
 
 @ObjectType()
 @Entity()
@@ -21,5 +23,16 @@ export class Freelancer {
   @Field({ nullable: true })
   phone?: string;
 
- 
+  @OneToMany(() => Competence, c => c.freelancer)
+  @Field(() => [Competence], { nullable: true })
+  competences?: Competence[];
+
+  @OneToMany(() => LienProfessionnel, l => l.freelancer)
+  @Field(() => [LienProfessionnel], { nullable: true })
+  liens?: LienProfessionnel[];
+
+  @ManyToMany(() => Mission, mission => mission.freelancer)
+  @JoinTable()
+  @Field(() => [Mission], { nullable: true })
+  missions?: Mission[];
 } 
