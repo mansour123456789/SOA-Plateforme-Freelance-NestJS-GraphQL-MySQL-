02d0b015b1408118ad8c25be6becdc62f3e27781
houssemJammeli <houssemjammeli037@gmail.com>
2025-06-08 13:25:41 +0100

    module competence

--- src/competence/competence.module.ts
+++ src/competence/competence.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CompetenceService } from './competence.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Competence } from './competence.entity';
 import { CompetenceResolver } from './competence.resolver';
+import { CompetenceService } from './competence.service';
+import { Freelancer } from '../freelancer/entities/freelancer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Competence, Freelancer])],
   providers: [CompetenceResolver, CompetenceService],
 })
 export class CompetenceModule {}
